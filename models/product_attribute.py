1587751cdad90d09b8cb3148bc674b504a0ca3ab
Pafnow <pafnow@hotmail.com>
2020-08-24 13:06:21 +0700

    add auto_add feature on attribute values

--- models/product_attribute.py
+++ models/product_attribute.py
@@ -11,9 +11,25 @@ class ProductAttribute(models.Model):
                                           help="Force selection of a default value for this attribute on Variants tab of product template form")
 
 
+class ProductAttributeValue(models.Model):
+    _inherit = "product.attribute.value"
+
+    is_auto_add = fields.Boolean("Is auto added", help="Automatically add this value when the attribute is selected on a product template")
+
+
 class ProductTemplateAttributeLine(models.Model):
     _inherit = "product.template.attribute.line"
 
+    @api.onchange('attribute_id')
+    def _onchange_attribute_id(self):
+        super()._onchange_attribute_id()
+        #Add auto_add values
+        if not self.value_ids:
+            pav_aa = self.env['product.attribute.value'].search([('attribute_id', '=', self.attribute_id.id), ('is_auto_add', '=', True)])
+            if pav_aa:
+                self.value_ids = [(4, r.id) for r in pav_aa]
+
+    # Default Value required
     default_value_id = fields.Many2one('product.attribute.value', string="Default Value", domain="[('id', 'in', value_ids)]")
 
     @api.constrains('default_value_id')
